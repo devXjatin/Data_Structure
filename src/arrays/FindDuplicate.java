ff050f6a460322aa26056d6bbbce31cf1f97f1de
devXjatin <jatin13280@gmail.com>
2022-01-19 18:20:59 +0530

    find duplicate element in the array

---
+++ src/arrays/FindDuplicate.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class FindDuplicate {
+
+    //method to find duplicate in the array
+    static int findDuplicate(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int j = 0;
+            while (j < arr.length){
+                if(i != j){
+                    if(arr[i] == arr[j]){
+                        return arr[i];
+                    }
+                }
+                j++;
+            }
+
+        }
+        return Integer.MAX_VALUE;
+    }
+    public static void main(String[] args) {
+        int [] arr = {2, 4, 5, 1, 2, 9};
+        System.out.println(findDuplicate(arr));
+    }
+}
