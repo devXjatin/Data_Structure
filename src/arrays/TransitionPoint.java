4b09741f92e04f5c8559e7290dc22f1f1b0fa833
devXjatin <jatin13280@gmail.com>
2022-01-22 18:06:25 +0530

    transition point method defination

--- src/arrays/TransitionPoint.java
+++ src/arrays/TransitionPoint.java
@@ -5,7 +5,7 @@ public class TransitionPoint {
     static int transitionPoint(int[] arr){
         for(int i =0; i<arr.length; i++){
             if(arr[i] == 1){
-                return i;
+                return i+1;
             }
         }
         return -1;
