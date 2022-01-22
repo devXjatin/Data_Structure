cde2e512e5c26e6c119d8009a2b89b4b9803a6ac
devXjatin <jatin13280@gmail.com>
2022-01-22 18:04:17 +0530

    transition point method declare

--- src/arrays/TransitionPoint.java
+++ src/arrays/TransitionPoint.java
@@ -1,7 +1,12 @@
 package arrays;
 
 public class TransitionPoint {
+
+    static int transitionPoint(int[] arr){
+        return;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 0, 1, 0, 1, 1, 0};
+        transitionPoint(arr);
     }
 }
