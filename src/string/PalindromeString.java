683eb32cea94b703d997972d34a797b1bab25c4a
devXjatin <jatin13280@gmail.com>
2022-01-26 15:43:07 +0530

    check palindrom string using approach 2

--- src/string/PalindromeString.java
+++ src/string/PalindromeString.java
@@ -3,12 +3,27 @@ package string;
 public class PalindromeString {
     //check given string is palindrome or not
     static boolean isPalindromeString(String str){
-        String reverseString = "";
+
+        //Approach 1
+       /* String reverseString = "";
         for (int i = str.length()-1; i >=0 ; i--) {
             reverseString += str.charAt(i);
         }
 
-        return str.equals(reverseString);
+        return str.equals(reverseString);*/
+
+        //Approach 2
+        int start = 0;
+        int end = str.length()-1;
+        while(start<end){
+            if(str.charAt(start) != str.charAt(end)){
+                return false;
+            }else{
+                start++;
+                end--;
+            }
+        }
+        return true;
     }
     public static void main(String[] args) {
         String str = "abcdcba";
