5581f37f3fd4d5a10f68f532c9670077883148d0
devXjatin <jatin13280@gmail.com>
2022-01-28 15:04:39 +0530

    calculate power of given number using recursion

---
+++ src/recursion/CalculatePower.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class CalculatePower {
+    static int calculatePower(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        return x * calculatePower(3, n-1);
+    }
+
+    public static void main(String[] args) {
+        int value = 3;
+        int power = 4;
+        System.out.println(calculatePower(3,4));
+    }
+}
