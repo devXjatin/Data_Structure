5da558f3fdd100c268b3fd0473d7e7d5459f3d0d
devXjatin <jatin13280@gmail.com>
2022-01-28 16:28:55 +0530

    check number in the array

---
+++ src/recursion/CheckNumberInArray.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class CheckNumberInArray {
+    static boolean checkNumber(int[] arr, int startIndex, int search){
+        if(startIndex == arr.length){
+            return false;
+        }
+        if(arr[startIndex] == search){
+            return true;
+        }
+        return checkNumber(arr, startIndex+1, search);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 10, 5, 46, 30, 23};
+        System.out.println(checkNumber(arr, 0, 40));
+    }
+}
