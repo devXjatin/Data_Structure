64e94b183ac2022e3ed5ac4684bdccde5548ddb6
devXjatin <jatin13280@gmail.com>
2022-01-28 16:25:24 +0530

    sum of array

---
+++ src/recursion/ArraySum.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class ArraySum {
+    static int sumOfArray(int[] arr, int startIndex){
+        if(startIndex == arr.length){
+            return 0;
+        }
+        return arr[startIndex]+sumOfArray(arr, startIndex+1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3,4 ,5};
+        System.out.println(sumOfArray(arr, 0));
+    }
+}
