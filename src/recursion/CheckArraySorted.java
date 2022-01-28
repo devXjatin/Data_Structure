b4f5d686b495fb288b319fdbd765a25447baf1b9
devXjatin <jatin13280@gmail.com>
2022-01-28 16:36:32 +0530

    check array sorted without using extra space using ternary operator

--- src/recursion/CheckArraySorted.java
+++ src/recursion/CheckArraySorted.java
@@ -1,22 +1,22 @@
 package recursion;
 
 public class CheckArraySorted {
-    static boolean isSortedArray(int[] arr){
-        if(arr.length <=1){
+    static boolean isSortedArray(int[] arr, int startIndex){
+        if(startIndex == arr.length-1 ){
             return true;
         }
-        if(arr[0]>arr[1]){
-            return false;
-        }
-        int[] smallInput = new int[arr.length-1];
-        for (int i = 1; i < arr.length; i++) {
-            smallInput[i-1] = arr[i];
-        }
-        return isSortedArray(smallInput);
+//        if(arr[0]>arr[1]){
+//            return false;
+//        }
+//        int[] smallInput = new int[arr.length-1];
+//        for (int i = 1; i < arr.length; i++) {
+//            smallInput[i-1] = arr[i];
+//        }
+        return arr[startIndex] > arr[startIndex+1]? false:isSortedArray(arr, startIndex+1);
     }
 
     public static void main(String[] args) {
         int[] arr ={1, 2, 3, 4, 5};
-        System.out.println(isSortedArray(arr));
+        System.out.println(isSortedArray(arr, 0));
     }
 }
