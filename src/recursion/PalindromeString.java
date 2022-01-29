e23be38c37abab735bc9c294e820c6907730fbf7
devXjatin <jatin13280@gmail.com>
2022-01-29 14:39:08 +0530

    check if string is palindrome or not using recursion

---
+++ src/recursion/PalindromeString.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class PalindromeString {
+    private static boolean palindromeString(String str, int start, int end){
+        if(start >= end){
+            return true;
+        }
+        if(str.charAt(start) != str.charAt(end)){
+            return false;
+        }
+        return palindromeString(str, start+1, end-1);
+
+    }
+    static boolean palindromeString(String str){
+        return palindromeString(str, 0, str.length()-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindromeString("racecar"));
+
+    }
+}
