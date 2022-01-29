60c77ed5dc7965e238a4b51389a0d7f10f23ed6d
devXjatin <jatin13280@gmail.com>
2022-01-29 14:17:13 +0530

    find all indices of searching element

---
+++ src/recursion/AllIndices.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class AllIndices {
+    private static int[] allIndices(int[] arr, int startIndex, int search, int foundSoFar){
+        if(startIndex == arr.length){
+            return new int[foundSoFar];
+        }
+        if(arr[startIndex] == search){
+            int[] indices = allIndices(arr, startIndex+1, search, foundSoFar+1);
+            indices[foundSoFar] = startIndex+1;
+            return indices;
+        }else{
+            int[] indices = allIndices(arr, startIndex+1, search, foundSoFar);
+            return indices;
+        }
+    }
+
+    static int[] allIndices(int[]arr, int search){
+        return allIndices(arr, 0, search, 0);
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,3,6,1,7};
+        int[] indices = allIndices(arr, 0);
+        for(var val: indices){
+            System.out.print(val+" ");
+        }
+    }
+}
