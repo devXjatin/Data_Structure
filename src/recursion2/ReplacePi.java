ea4f9fdc8b984247c782ea4f6549310c43bb992b
devXjatin <jatin13280@gmail.com>
2022-01-31 14:19:05 +0530

    replace pi by 3.14

---
+++ src/recursion2/ReplacePi.java
@@ -0,0 +1,19 @@
+package recursion2;
+
+public class ReplacePi {
+    static String replacePi(String str){
+        if(str.length()<=1){
+            return str;
+        }
+        if(str.charAt(0) =='p' && str.charAt(1) == 'i'){
+            return "3.14"+replacePi(str.substring(2));
+        }else{
+            return str.charAt(0) + replacePi(str.substring(1));
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "pip";
+        System.out.println(replacePi(str));
+    }
+}
