3ec077ad2801c97ce1f06a69291a1c08a5835d17
devXjatin <jatin13280@gmail.com>
2022-02-01 14:22:43 +0530

    check ab problem

---
+++ src/recursion3/CheckAb.java
@@ -0,0 +1,22 @@
+package recursion3;
+
+public class CheckAb {
+    static boolean checkAB(String str){
+        if(str.length()==0){
+            return true;
+        }
+        if(str.charAt(0)=='a'){
+            if(str.substring(1).length() >1 && str.substring(1,3).equals("bb")){
+                return checkAB(str.substring(3));
+            }
+            else{
+                return checkAB(str.substring(1));
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String str = "abbabb";
+        System.out.println(checkAB(str));
+    }
+}
