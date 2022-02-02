7fe7687087c1c3943c9b67edf66aa8728dc7b177
devXjatin <jatin13280@gmail.com>
2022-02-02 18:04:44 +0530

    optimise input from O(n^2) to O(n)

--- src/linkedlist1/takeinput/LinkedListUse.java
+++ src/linkedlist1/takeinput/LinkedListUse.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 public class LinkedListUse {
     static Scanner scn = new Scanner(System.in);
     //take user input in linkedlist
-    static Node<Integer> takeInput(){
-        Node<Integer> head = null;
+   public static Node<Integer> takeInput(){
+        Node<Integer> head = null, tail = null;
         int data = scn.nextInt();
         while(data != -1){
             Node<Integer> currentNode = new Node<>(data);
             if(head == null){
                 head=currentNode;
+                tail = currentNode;
             }else{
-                Node<Integer> tail = head;
+               /* Node<Integer> tail = head;
                 while(tail.next != null){
                     tail = tail.next;
-                }
+                }*/
                 tail.next = currentNode;
+                tail = currentNode;
             }
             data = scn.nextInt();
         }
@@ -25,7 +27,7 @@ public class LinkedListUse {
     }
 
     //print linkedlist
-    static void print(Node<Integer> head){
+    public static void print(Node<Integer> head){
         Node<Integer> temp = head;
         while(temp != null){
             System.out.print(temp.data+"->");
