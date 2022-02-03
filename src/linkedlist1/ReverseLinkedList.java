b970139de27f9f476b08a4f5e38e11e522d82278
devXjatin <jatin13280@gmail.com>
2022-02-03 17:13:34 +0530

    reverse linkedlit

---
+++ src/linkedlist1/ReverseLinkedList.java
@@ -0,0 +1,17 @@
+package linkedlist1;
+
+public class ReverseLinkedList {
+    //reverse linkedlist
+    static void printReverse(Node<Integer> head){
+        if(head == null){
+            return;
+        }
+        printReverse(head.next);
+        System.out.print(head.data+" ");
+    }
+    public static void main(String[] args) {
+        Node<Integer> head = TakeUserInput.takeInput();
+        printReverse(head);
+        TakeUserInput.print(head);
+    }
+}
