49a6b071692ef966c3085687f19cda245ffe403c
devXjatin <jatin13280@gmail.com>
2022-02-07 16:00:52 +0530

    reverse queue using recursion

---
+++ src/queue/QueueReverse.java
@@ -0,0 +1,28 @@
+package queue;
+
+import java.util.*;
+
+public class QueueReverse<T> {
+    public static void queueReverse(Queue<Integer> queue){
+        if(queue.size() == 0){
+            return;
+        }
+        int temp = queue.poll();
+        queueReverse(queue);
+        queue.add(temp);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40, 50};
+        Queue<Integer> queue = new LinkedList<>();
+        for(var element: arr){
+            queue.add(element);
+        }
+
+        queueReverse(queue);
+
+        while (!queue.isEmpty()){
+            System.out.print(queue.poll()+" ");
+        }
+    }
+}
