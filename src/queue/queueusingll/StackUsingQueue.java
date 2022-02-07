539d5f9e9134b508338d63548b99d4b8f9f82c60
devXjatin <jatin13280@gmail.com>
2022-02-07 15:48:52 +0530

    stack using 2 queue

---
+++ src/queue/queueusingll/StackUsingQueue.java
@@ -0,0 +1,54 @@
+package queue.queueusingll;
+
+import java.util.*;
+
+public class StackUsingQueue<T> {
+    private Queue<T> queue1;
+    private Queue<T> queue2;
+    private int size;
+
+    public StackUsingQueue(){
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public void push(T element){
+        while(!queue1.isEmpty()){
+            queue2.add(queue1.poll());
+        }
+        queue1.add(element);
+        size++;
+        while (!queue2.isEmpty()){
+            queue1.add(queue2.poll());
+        }
+    }
+    public T peek(){
+        return queue1.peek();
+    }
+
+    public T pop(){
+        size--;
+        return queue1.poll();
+
+    }
+
+    public static void main(String[] args) {
+        StackUsingQueue<Integer> stack = new StackUsingQueue<>();
+        int[] arr = {10, 20, 30, 40, 50};
+        for(var ele:arr){
+            stack.push(ele);
+        }
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+    }
+}
