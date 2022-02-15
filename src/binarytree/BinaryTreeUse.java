7d2a45a047b285bc171453cfed65c69f195f69bd
devXjatin <jatin13280@gmail.com>
2022-02-15 19:54:47 +0530

    take user input and print detailed waise

--- src/binarytree/BinaryTreeUse.java
+++ src/binarytree/BinaryTreeUse.java
@@ -1,7 +1,42 @@
 package binarytree;
 
-public class BinaryTreeUse {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class BinaryTreeUse {
+    final static Scanner scn = new Scanner(System.in);
+
+    static BinaryTreeNode<Integer> takeTreeInput(){
+        System.out.println("Enter root data: ");
+        int rootData = scn.nextInt();
+        if(rootData==-1){
+            return null;
+        }
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+        BinaryTreeNode<Integer> leftChild = takeTreeInput();
+        BinaryTreeNode<Integer> rightChild = takeTreeInput();
+        root.left = leftChild;
+        root.right=rightChild;
+        return root;
+
+    }
+
+    static void printDetailed(BinaryTreeNode<Integer> root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+": ");
+        if(root.left !=null){
+            System.out.print("L"+root.left.data+",");
+        }
+        if(root.right !=null){
+            System.out.print("R"+root.right.data);
+        }
+        printDetailed(root.left);
+        printDetailed(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = takeTreeInput();
+        printDetailed(root);
     }
 }
