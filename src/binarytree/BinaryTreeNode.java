3b1d79186c4f3745de7739302c5d7b1e118d12ae
devXjatin <jatin13280@gmail.com>
2022-02-15 17:35:02 +0530

    binary tree node class

---
+++ src/binarytree/BinaryTreeNode.java
@@ -0,0 +1,10 @@
+package binarytree;
+
+public class BinaryTreeNode<T> {
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+    BinaryTreeNode(T data){
+        this.data = data;
+    }
+}
