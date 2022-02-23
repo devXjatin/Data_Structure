84e7d3f862932480702e705df21f915dafe0db37
devXjatin <jatin13280@gmail.com>
2022-02-23 20:41:44 +0530

    lowest common ancestor in binary searcg tree

---
+++ src/binarysearchtree/LowestCommonAncestor.java
@@ -0,0 +1,27 @@
+package binarysearchtree;
+
+import binarytree.BinaryTreeNode;
+import binarytree.BinaryTreeUse;
+
+public class LowestCommonAncestor {
+
+    static BinaryTreeNode<Integer> lowestCommonAncestor(BinaryTreeNode<Integer> root, BinaryTreeNode<Integer> p,
+                                                        BinaryTreeNode<Integer> q){
+        if(root == null){
+            return root;
+        }
+        if(root.data >p.data && root.data>q.data){
+            return lowestCommonAncestor(root.left, p,q);
+        }
+        if(root.data < p.data && root.data < q.data){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = BinaryTreeUse.takeTreeInputBetter(true, 0, true);
+        BinaryTreeNode<Integer>lca = lowestCommonAncestor(root, new BinaryTreeNode<>(2),new BinaryTreeNode<>(5));
+        System.out.println(lca.data);
+    }
+}
