4599f447517301ba3d04a513b7474e7b7afb438d
devXjatin <jatin13280@gmail.com>
2022-03-02 23:46:52 +0530

    search method

--- src/tries/TrieNode.java
+++ src/tries/TrieNode.java
@@ -18,6 +18,10 @@ class Trie{
     }
 
     private void add(TrieNode root, String word){
+        if(word.length()==0){
+            root.isTerminating = true;
+            return;
+        }
 
         int childIndex = word.charAt(0)-'a';
         TrieNode child = root.children[childIndex];
@@ -28,9 +32,27 @@ class Trie{
         add(child, word.substring(1));
     }
 
+    //search
+    private boolean search(TrieNode root, String word){
+        if(word.length()==0){
+            return root.isTerminating;
+        }
+        int childIndex = word.charAt(0)-'a';
+        TrieNode child = root.children[childIndex];
+        if(child == null){
+            return false;
+        }
+        return search(child, word.substring(1));
+    }
+
     //add in trie
     public void add(String word){
         add(root, word);
+    }
+
+    //search word in trie
+    public boolean search(String word){
+        return search(root, word);
     }
 
 }
