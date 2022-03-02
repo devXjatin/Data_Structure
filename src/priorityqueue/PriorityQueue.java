6c2c823c92a1a0966e742cc5366b36d327df0557
devXjatin <jatin13280@gmail.com>
2022-03-02 18:32:39 +0530

    isempty, size function

---
+++ src/priorityqueue/PriorityQueue.java
@@ -0,0 +1,22 @@
+package priorityqueue;
+
+import java.util.*;
+
+public class PriorityQueue {
+    private ArrayList<Integer> heap;
+
+    public PriorityQueue(){
+        heap = new ArrayList<>();
+    }
+
+    //check arraylist is empty or not
+    boolean isEmpty(){
+        return heap.size() == 0;
+    }
+
+    //return size of the heap
+    int size(){
+        return heap.size();
+    }
+
+}
