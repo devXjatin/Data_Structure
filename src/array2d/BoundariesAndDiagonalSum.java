9ce68db267f3e427c7708ebb36cc7a28a344ad88
devXjatin <jatin13280@gmail.com>
2022-05-15 23:21:34 +0530

    BoundariesAndDiagonalSum

--- src/array2d/BoundariesAndDiagonalSum.java
+++ src/array2d/BoundariesAndDiagonalSum.java
@@ -3,7 +3,7 @@ package array2d;
 public class BoundariesAndDiagonalSum {
 
     //calculate total sum of boundaries and diagonal
-    static int boundraiesAndDiagonalSum(int[][] arr){
+    static int boundraiesAndDiagonalSum(int[][] arr) {
         int rows = arr.length;
         int cols = arr[0].length;
         int sum = 0;
@@ -12,17 +12,16 @@ public class BoundariesAndDiagonalSum {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 //calculate diagonal sum
-                if(i == j){
-                    sum+=arr[i][j];
+                if (i == j) {
+                    sum += arr[i][j];
                 }
                 //calculate boundaries sum
                 else {
-                    sum+= arr[i][j];
+                    sum += arr[i][j];
                 }
             }
         }
         return sum;
-
     }
     public static void main(String[] args) {
         int[][] arr2d = {{1,2,3}, {4,5,6},{7,8,9}};
